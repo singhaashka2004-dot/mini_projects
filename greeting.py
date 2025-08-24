a1c05b725b91b604d29c4154ee41937b9c72a7e3
Aashka Singh <singhaashka2004@gmail.com>
2025-08-24 13:26:49 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,10 @@
+def main():
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+
+    full_name = first_name + " " + last_name
+
+    print(f"Hello, {full_name}! Welcome!")
+
+if __name__ == "__main__":
+    main()
