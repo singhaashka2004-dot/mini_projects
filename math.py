f08515fa273cd787b704c35b3b26c49b300bff94
Aashka Singh <singhaashka2004@gmail.com>
2025-08-30 17:48:37 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sin_val = math.sin(num)
+
+print("Square root of", num, "is", sqrt_val)
+print("Natural log of", num, "is", log_val)
+print("Sine of", num, "is", sin_val)
